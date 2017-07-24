223502ab6cf4eeb3e43b2c62efba3dea2652e053
David Peicho <david.peicho@gmail.com>
2017-07-25 00:56:13 +0200

    main: Add Logger class
    - Adds Logger profiling execution time.

---
+++ include/utils/logger.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+#include <utils/singleton.hpp>
+
+namespace albedo
+{
+
+namespace tools
+{
+
+namespace utils
+{
+
+class Logger : public Singleton<Logger>
+{
+  public:
+    inline void
+    start(std::string message)
+    {
+      std::cout << message << std::endl;
+      this->start();
+    }
+
+    inline void
+    start()
+    {
+      begin_ = std::chrono::high_resolution_clock::now();
+      auto time_begin = std::chrono::high_resolution_clock::now();
+    }
+
+    inline void
+    stop(std::string message)
+    {
+      auto end = std::chrono::high_resolution_clock::now();
+      auto elapsed = end - begin_;
+      auto seconds = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
+      std::cout << message << (seconds.count() * 0.001f) << 's' << std::endl;
+    }
+
+  private:
+    std::chrono::high_resolution_clock::time_point begin_;
+
+};
+
+} // namespace utils
+
+} // namespace tools
+
+} // namespace albedo
