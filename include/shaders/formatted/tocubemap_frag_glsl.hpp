a40bff4e2e8e39b08f9f47e7201a8758f9c7e342
David Peicho <david.peicho@gmail.com>
2017-07-25 00:03:28 +0200

    processor: Fix GPUProcessor Cubemap symetry
    - Fixes invalid Cubemap symetry

--- include/shaders/formatted/tocubemap_frag_glsl.hpp
+++ include/shaders/formatted/tocubemap_frag_glsl.hpp
@@ -1,15 +1,18 @@
 #include <GL/gl.h>
 static const GLchar *shader_source_tocubemap_frag_glsl = {
 "#version 330 core\n"
+"#define PI 3.1415926535897932384626433832795\n"
 "in vec3 fragPos;\n"
 "out vec4 fragColor;\n"
 "uniform sampler2D uMap;\n"
 "const vec2 invAtan = vec2(0.1591, 0.3183);\n"
 "void main() {\n"
 "    vec3 fetch = normalize(fragPos);\n"
-"    vec2 uv = vec2(atan(fetch.z, fetch.x), asin(fetch.y));\n"
-"    uv *= invAtan;\n"
-"    uv += 0.5;\n"
+"    vec2 uv = vec2(atan(fetch.x, fetch.z), acos(fetch.y));\n"
+"    uv.x = (PI + uv.x) * (0.5f / PI);\n"
+"    uv.y = uv.y * invAtan.y;\n"
+"    //uv *= invAtan;\n"
+"    //uv += 0.5;\n"
 "    fragColor = vec4(texture(uMap, uv).rgb, 1.0);\n"
 "}\n"
 };
--- src/shaders/tocubemap_frag.glsl
+++ src/shaders/tocubemap_frag.glsl
@@ -1,5 +1,7 @@
 #version 330 core
 
+#define PI 3.1415926535897932384626433832795
+
 in vec3 fragPos;
 out vec4 fragColor;
 
@@ -10,9 +12,10 @@ const vec2 invAtan = vec2(0.1591, 0.3183);
 void main() {
 
     vec3 fetch = normalize(fragPos);
-    vec2 uv = vec2(atan(fetch.z, fetch.x), asin(fetch.y));
-    uv *= invAtan;
-    uv += 0.5;
+    vec2 uv = vec2(atan(fetch.x, fetch.z), acos(fetch.y));
+
+    uv.x = (PI + uv.x) * (0.5f / PI);
+    uv.y = uv.y * invAtan.y;
 
     fragColor = vec4(texture(uMap, uv).rgb, 1.0);
 
